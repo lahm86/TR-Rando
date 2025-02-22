f9c9b4858aeb56bf2d6c5484a77ad3162d50bfaa
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-02-22 12:38:40 +0000

    Start TR4/5 type utilities

---
+++ TRLevelControl/Helpers/TR4TypeUtilities.cs
@@ -0,0 +1,68 @@
+﻿using TRLevelControl.Model;
+
+namespace TRLevelControl.Helpers;
+
+public static class TR4TypeUtilities
+{
+    private static readonly List<TR4Type> _pickupTypes = new()
+    {
+        TR4Type.PistolsItem,
+        TR4Type.PistolsAmmoItem,
+        TR4Type.ShotgunItem,
+        TR4Type.ShotgunAmmo1Item,
+        TR4Type.ShotgunAmmo2Item,
+        TR4Type.UziItem,
+        TR4Type.UziAmmoItem,
+        TR4Type.SixshooterItem,
+        TR4Type.SixshooterAmmoItem,
+        TR4Type.CrossbowItem,
+        TR4Type.CrossbowAmmo1Item,
+        TR4Type.CrossbowAmmo2Item,
+        TR4Type.CrossbowAmmo3Item,
+        TR4Type.LasersightItem,
+        TR4Type.GrenadeGunItem,
+        TR4Type.GrenadeGunAmmo1Item,
+        TR4Type.GrenadeGunAmmo2Item,
+        TR4Type.GrenadeGunAmmo3Item,
+        TR4Type.FlareInvItem,
+        TR4Type.LargeMed,
+        TR4Type.SmallMed,
+        TR4Type.Waterskin1Empty,
+        TR4Type.Waterskin2Empty,
+    };
+
+    private static readonly List<TR4Type> _doorTypes = new()
+    {
+        TR4Type.DoorType1,
+        TR4Type.DoorType2,
+        TR4Type.DoorType3,
+        TR4Type.DoorType4,
+        TR4Type.DoorType5,
+        TR4Type.DoorType6,
+        TR4Type.DoorType7,
+        TR4Type.DoorType8,
+        TR4Type.PushpullDoor1,
+        TR4Type.PushpullDoor2,
+        TR4Type.KickDoor1,
+        TR4Type.KickDoor2,
+        TR4Type.UnderwaterDoor,
+        TR4Type.DoubleDoors,
+        TR4Type.Trapdoor1,
+        TR4Type.Trapdoor2,
+        TR4Type.Trapdoor3,
+        TR4Type.FloorTrapdoor1,
+        TR4Type.FloorTrapdoor2,
+        TR4Type.CeilingTrapdoor1,
+        TR4Type.CeilingTrapdoor2,
+        TR4Type.ScalingTrapdoor,
+    };
+
+    public static bool IsAnyPickupType(TR4Type type)
+    {
+        return _pickupTypes.Contains(type)
+        || (type >= TR4Type.PuzzleItem1 && type <= TR4Type.QuestItem6);
+    }
+
+    public static bool IsDoorType(TR4Type type)
+        => _doorTypes.Contains(type);
+}
---
+++ TRLevelControl/Helpers/TR5TypeUtilities.cs
@@ -0,0 +1,64 @@
+﻿using TRLevelControl.Model;
+
+namespace TRLevelControl.Helpers;
+
+public static class TR5TypeUtilities
+{
+    private static readonly List<TR5Type> _pickupTypes = new()
+    {
+        TR5Type.PistolsItem,
+        TR5Type.PistolsAmmoItem,
+        TR5Type.ShotgunItem,
+        TR5Type.ShotgunAmmo1Item,
+        TR5Type.ShotgunAmmo2Item,
+        TR5Type.UziItem,
+        TR5Type.UziAmmoItem,
+        TR5Type.HkItem,
+        TR5Type.HkAmmoItem,
+        TR5Type.RevolverItem,
+        TR5Type.RevolverAmmoItem,
+        TR5Type.CrossbowItem,
+        TR5Type.CrossbowAmmo1Item,
+        TR5Type.CrossbowAmmo2Item,
+        TR5Type.LasersightItem,
+        TR5Type.SilencerItem,
+        TR5Type.FlareInvItem,
+        TR5Type.BigmediItem,
+        TR5Type.SmallmediItem,
+    };
+
+    private static readonly List<TR5Type> _doorTypes = new()
+    {
+        TR5Type.DoorType1,
+        TR5Type.DoorType2,
+        TR5Type.DoorType3,
+        TR5Type.DoorType4,
+        TR5Type.DoorType5,
+        TR5Type.DoorType6,
+        TR5Type.DoorType7,
+        TR5Type.DoorType8,
+        TR5Type.PushpullDoor1,
+        TR5Type.PushpullDoor2,
+        TR5Type.KickDoor1,
+        TR5Type.KickDoor2,
+        TR5Type.UnderwaterDoor,
+        TR5Type.DoubleDoors,
+        TR5Type.Trapdoor1,
+        TR5Type.Trapdoor2,
+        TR5Type.Trapdoor3,
+        TR5Type.FloorTrapdoor1,
+        TR5Type.FloorTrapdoor2,
+        TR5Type.CeilingTrapdoor1,
+        TR5Type.CeilingTrapdoor2,
+        TR5Type.ScalingTrapdoor,
+    };
+
+    public static bool IsAnyPickupType(TR5Type type)
+    {
+        return _pickupTypes.Contains(type)
+        || (type >= TR5Type.PuzzleItem1 && type <= TR5Type.BurningTorchItem);
+    }
+
+    public static bool IsDoorType(TR5Type type)
+        => _doorTypes.Contains(type);
+}
